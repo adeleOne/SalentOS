b89aa73bcc68c2c1e9da96f0c1ba8e2bdeab8273
rciotola <rciotola@gmail.com>
2014-06-09 12:57:56 +0200

    Update script_salentos_post_installa_ITA_v1.sh

--- script_salentos_post_installa_ITA_v1.sh
+++ script_salentos_post_installa_ITA_v1.sh
@@ -4,7 +4,7 @@
 # Questo script è free software: si può modificare/ridistribuire 
 # sotto licenza # GNU General Public 
 # Una copia della Licenza GNU General Public License 
-# è visibile al link https://www.gnu.org/licenses/gpl-3.0.txt
+# è visibile al link
 #
 #
 #Definizione funzione testata
