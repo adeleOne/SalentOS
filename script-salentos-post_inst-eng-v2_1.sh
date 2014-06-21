0eafa635897df6fb679fe870d1948337ef930e00
rciotola <rciotola@gmail.com>
2014-06-21 16:28:46 +0200

    Update script-salentos-post_inst-eng-v2_1.sh

--- script-salentos-post_inst-eng-v2_1.sh
+++ script-salentos-post_inst-eng-v2_1.sh
@@ -25,7 +25,7 @@ function scelta()
 {
 A='a'
 until [[ $A = "Y" || $A = "N"  || $A = "n" || $A = "y" ]]; do
-     read -p "$B - Confirm (Y)/(N) ?" A
+     read -p "$B - Confirm (Y)/(N) ?"A
      done
 }
 clear
@@ -100,7 +100,7 @@ fi
 #Installazione gnome-screenshot
 clear
 testa
-B="INSTALL  GNOME-SCREENSHOT (Save the screenshots)"
+B='INSTALL GNOME-SCREENSHOT"
 scelta
 if  [[ $A = 'Y' || $A = 'y' ]]; then   
     dpkg -s gnome-screenshot &>/dev/null        
